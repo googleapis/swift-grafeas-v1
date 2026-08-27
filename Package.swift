8be50efe8c0263fdc137739fbf538cfa9ead4297
Carlos O'Ryan <coryan@google.com>
2026-08-27 15:52:38 +0000

    cleanup: rename generated directories (#546)

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     .package(path: "../../packages/auth"),
     .package(path: "../../packages/gax"),
     .package(path: "../../packages/wkt"),
-    .package(path: "../../generated/google-rpc"),
+    .package(path: "../../generated/swift-google-rpc"),
     .package(url: "https://github.com/apple/swift-log", from: "1.12.0"),
   ],
   targets: [
@@ -37,7 +37,7 @@ let package = Package(
         .product(name: "GoogleCloudAuth", package: "auth"),
         .product(name: "GoogleCloudGax", package: "gax"),
         .product(name: "GoogleCloudWKT", package: "wkt"),
-        .product(name: "GoogleRpc", package: "google-rpc"),
+        .product(name: "GoogleRpc", package: "swift-google-rpc"),
         .product(name: "Logging", package: "swift-log"),
       ],
     )
